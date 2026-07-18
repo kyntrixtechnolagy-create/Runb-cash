f2ac97d46d981db5fb3998d392bf039c51bcf277
Tally POS Developer <your-email@example.com>
2026-07-18 11:58:12 +0530

    Enable foreground push notifications in Capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.vercel.runb_cash.twa',
   appName: 'Runb',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
 };
 
 export default config;
