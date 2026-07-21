1de6c87c4e39341713c947d9b6b6396a38e64327
Tally POS Developer <your-email@example.com>
2026-07-21 17:24:31 +0530

    Update app name to RunB

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.vercel.runb_cash.twa',
-  appName: 'Runb',
+  appName: 'RunB',
   webDir: 'dist',
   plugins: {
     PushNotifications: {
